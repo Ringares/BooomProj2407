5567e1cb8b2e01e6681152fc54102f442d10fdae
ringares <frank.linsh0808@gmail.com>
2024-08-15 00:57:14 +0800

    adjust level_temp

--- scene/game/entity/entity_constants.gd
+++ scene/game/entity/entity_constants.gd
@@ -11,7 +11,7 @@ enum ENTITY_TYPE {
 	HEALTH_UP = 5,
 	STRENGTH_UP = 6,
 	EXIT = 7, # 出口
-	RECYCLING = 8, # 回收事件，回收能量
+	RECYCLER = 8, # 回收事件，回收能量
 	ENERGY_UP = 9,
 	ENERGY_ROOM_UP = 10,
 	INVENTORY_ROOM_UP = 11,
@@ -34,12 +34,12 @@ static var EntityMap = {
 	ENTITY_TYPE.HEALTH_UP: "res://scene/game/entity/property_item/item_hp_up.tres",
 	ENTITY_TYPE.STRENGTH_UP: "res://scene/game/entity/property_item/item_str_up.tres",
 	ENTITY_TYPE.EXIT: "res://scene/game/entity/func_item/item_exit.tres",
-	ENTITY_TYPE.RECYCLING: null,
+	ENTITY_TYPE.RECYCLER: "res://scene/game/entity/func_item/item_recycler.tres",
 	ENTITY_TYPE.ENERGY_UP: "res://scene/game/entity/property_item/item_energy_up.tres",
 	ENTITY_TYPE.ENERGY_ROOM_UP: "res://scene/game/entity/property_item/item_energy_capacity.tres",
 	ENTITY_TYPE.INVENTORY_ROOM_UP: "res://scene/game/entity/property_item/item_inven_capacity.tres",
 	ENTITY_TYPE.CHEST: "res://scene/game/entity/func_item/item_chest.tres",
-	ENTITY_TYPE.SWITCHER: null,
+	ENTITY_TYPE.SWITCHER: "res://scene/game/entity/func_item/item_switcher.tres",
 	
 	ENTITY_TYPE.ENERMY_A: "res://scene/game/entity/enermy/enermy_a.tres",
 	ENTITY_TYPE.ENERMY_B: "res://scene/game/entity/enermy/enermy_b.tres",

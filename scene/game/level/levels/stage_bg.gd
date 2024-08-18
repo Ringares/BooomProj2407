a020039fac844301b0fbfd27c9f41269059142d5
ringares <frank.linsh0808@gmail.com>
2024-08-18 10:51:25 +0800

    ui assets update

--- scene/game/level/levels/stage_bg.gd
+++ scene/game/level/levels/stage_bg.gd
@@ -12,5 +12,5 @@ func _ready():
 func _on_contain_node_ready():
 	used_rect = contain_tile.get_used_rect()
 	stage_placehold.custom_minimum_size = Vector2(used_rect.size.x * 128 , used_rect.size.y * 110)
-	global_position = contain_tile.global_position - Vector2(18,19)
+	global_position = contain_tile.global_position - Vector2(23,23)
 	
